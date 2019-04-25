69151ce9fe0825e93ee7a5b46937fe60302a3ce3
Ash Rhazaly <ashleighrhazaly@gmail.com>
2019-04-25 20:50:11 +0800

    Update the readme to include nodemon

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 
 
 1. Run  `npm install` to install all the packages
-2. Navigate to `localhost:4000`
+2. Type `nodemon src/index.js` to start the server.
+3. Navigate to `localhost:4000`
--- src/index.js
+++ src/index.js
@@ -5,23 +5,29 @@ let links = [{
   url: 'www.howtographql.com',
   description: 'Fullstack tutorial for GraphQL'
 }]
-
+// 1
+let idCount = links.length
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    // 2
     feed: () => links,
   },
-  // 3
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
-  }
+  Mutation: {
+    // 2
+    post: (parent, args) => {
+       const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
+  },
 }
 // 3
 const server = new GraphQLServer({
-  typeDefs: "./schema.graphql",
+  typeDefs: "./src/schema.graphql",
   resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
